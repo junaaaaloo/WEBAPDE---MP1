bc0dd9a3340a955873f793e65a3206b0bbf46327
junaaaaloo <jonal_ticug@dlsu.edu.ph>
2017-06-18 21:58:34 +0800

    Updated Views

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 $(document).ready(function () {
-    "use strict";
-    $('.container-active').show();
-    $('.container').hide();
     
-    $('.navIcon').click(function () { 
-        var panel = $(this).attr('data-container');
-        var sub = $(this).attr('data-message').toUpperCase();
-        
-        $('.container-active').hide();
-        $('.container-active').addClass('container');
-        $('.container-active').removeClass('container-active');
-        $('#subtitle').text(sub);
-        $("#" + panel).addClass('container-active');
-        $('.container-active').show();
-    })
 });
+
+function getRecentUsers () {
+    
+}
+
+function getRecentPosts () {
+    
+}
+
+function getRecentAlbums () {
+    
+}
+
+function getRecentImages () {
+    
+}
+
+function getCommentsOfPost (postId) {
+    
+}
+
+function getCertainPost (postId) {
+    
+}
+
+function getCertainUser (userId) {
+    
+}
+
+function getCertainImage (imageId) {
+    
+}
