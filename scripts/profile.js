a19c429a397f63c828f7d49ea447695162ca6eec
Janelle Gaba <janelle_gaba@dlsu.edu.ph>
2017-06-21 23:26:08 +0800

    profile recent posts

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,87 +1,91 @@
 var root = 'https://jsonplaceholder.typicode.com';
+var userPostCnt = 0;
 
 $(function() {
     var string = window.location.href;
     var url = new URL(string);
     var userID = url.searchParams.get("userId");
-    goToProfile(userID);
+    goToProfile(userID, userPostCnt);
 })
 
-function goToProfile (id) {
+function goToProfile (id, postNum) {
     $.ajax({
       url: root + '/users',
       method: 'GET'
     }).then(function(data) {
-        for(i = 0; i < data.length; i++)
-        {
-            if(id == data[i].id)
-            {
-                var name = document.createElement("p");
-                var username = document.createElement("p");
-                var email = document.createElement("p");
-                var address = document.createElement("p");
-                var phone = document.createElement("p");
-                var website = document.createElement("p");
-                var company = document.createElement("p");
-                
-                $(name).addClass("name");
-                $(username).addClass("username");
-                $(email).addClass("email");
-                $(address).addClass("address");
-                $(phone).addClass("phone");
-                $(website).addClass("website");
-                $(company).addClass("company");
-                
-                $('.profileBoxImportant').append(name);
-                $('.profileBoxImportant').append(username);
-                $('.profileBoxImportant').append(email);
-                $('.profileBoxImportant').append(address);
-                $('.profileBoxImportant').append(phone);
-                $('.profileBoxImportant').append(website);
-                $('.profileBoxImportant').append(company);
-                
-                name.innerHTML = data[i].name;
-                username.innerHTML = "@" + data[i].username;
-                email.innerHTML = data[i].email;
-                address.innerHTML = data[i].address.street + " " + data[i].address.suite + " " + data[i].address.city + " " + data[i].address.zipcode;
-                phone.innerHTML = data[i].phone;
-                website.innerHTML = data[i].website;
-                company.innerHTML = data[i].company.name + " " + data[i].company.catchPhrase + " " + data[i].company.bs;
-                
-                getPostsOfUser(data[i]);
-             }
-        } 
+        var name = document.createElement("p");
+        var username = document.createElement("p");
+        var email = document.createElement("p");
+        var address = document.createElement("p");
+        var phone = document.createElement("p");
+        var website = document.createElement("p");
+        var company = document.createElement("p");
+
+        $(name).addClass("name");
+        $(username).addClass("username");
+        $(email).addClass("email");
+        $(address).addClass("address");
+        $(phone).addClass("phone");
+        $(website).addClass("website");
+        $(company).addClass("company");
+
+        $('.profileBoxImportant').append(name);
+        $('.profileBoxImportant').append(username);
+        $('.profileBoxImportant').append(email);
+        $('.profileBoxImportant').append(address);
+        $('.profileBoxImportant').append(phone);
+        $('.profileBoxImportant').append(website);
+        $('.profileBoxImportant').append(company);
+
+        name.innerHTML = data[id-1].name;
+        username.innerHTML = "@" + data[id-1].username;
+        email.innerHTML = data[id-1].email;
+        address.innerHTML = data[id-1].address.street + " " + data[id-1].address.suite + " " + data[id-1].address.city + " " + data[id-1].address.zipcode;
+        phone.innerHTML = data[id-1].phone;
+        website.innerHTML = data[id-1].website;
+        company.innerHTML = data[id-1].company.name + " " + data[id-1].company.catchPhrase + " " + data[id-1].company.bs;
+
+        getPostsOfUser(data[id-1], postNum); 
     });
 }
 
-function getPostsOfUser(user) {
+function getPostsOfUser(user, start) {
     $.ajax({
       url: root + '/posts?userId=' + user.id ,
       method: 'GET'
     }).then(function(data) {
-        for(i = 0; i < data.length; i++)
-        {
-            var post = document.createElement("div");
-            var picture = document.createElement("div");
-            var pTitle = document.createElement("p");
-            var contentPost = document.createElement("span");
-            var contentArea = document.createElement("div");
-                        
-            $(post).addClass("post");
-            $(picture).addClass("picture");
-            $(pTitle).addClass("postTitle");
-            $(contentPost).addClass("contentPost");
-            $(contentArea).addClass("contentArea");
-            
-            $(post).append(picture);
-            $(post).append(pTitle);
-            $(post).append('<br>');
-            $(post).append(contentArea);
-            $(contentArea).append(contentPost);
-            $(".postsProfileContainer").append(post);
+        if(data.length != start){
+            for(i = start; i < (start +5); i++)
+            {
+                var post = document.createElement("div");
+                var picture = document.createElement("div");
+                var pTitle = document.createElement("p");
+                var contentPost = document.createElement("span");
+                var contentArea = document.createElement("div");
 
-            contentPost.innerHTML = data[i].body;
-            pTitle.innerHTML = data[i].title;
-        } 
+                $(post).addClass("post");
+                $(picture).addClass("picture");
+                $(pTitle).addClass("postTitle");
+                $(contentPost).addClass("contentPost");
+                $(contentArea).addClass("contentArea");
+
+                $(post).append(picture);
+                $(post).append(pTitle);
+                $(post).append('<br>');
+                $(post).append(contentArea);
+                $(contentArea).append(contentPost);
+                $(".postsProfileContainer").append(post);
+
+                contentPost.innerHTML = data[i].body;
+                pTitle.innerHTML = data[i].title;
+            }
+            userPostCnt = i;
+            $('.more').click(function(e) {
+               getPostsOfUser(user, userPostCnt); 
+            });
+        }
+        else{
+            $('.more').hide();
+        }
     });
 }
