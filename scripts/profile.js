67c578f8fcac3536b973deaf099e7281bd1f0587
junaaaaloo <jonal_ticug@dlsu.edu.ph>
2017-06-27 23:57:27 +0800

    Fixed Google Maps bug, final edit

--- scripts/profile.js
+++ scripts/profile.js
@@ -7,14 +7,11 @@ var albumsCount = 0;
 
 $(document).ajaxStart(function(){
     $("#waitImg").css("display", "block");
-    if(postsCount == 0  && albumsCount == 0)
-        $('#wholePanel').css("display", "none");
 });
 
 $(document).ajaxComplete(function(){
     $("#waitImg").css("display", "none");
     $('#wholePanel').css("display", "block");
-    
 });
 
 
@@ -32,6 +29,9 @@ $(function() {
     else
         window.location.href = "users.html"  
     
+    $("#addressMap").resize(function() {
+        google.maps.event.trigger(map, 'resize');
+    });
     
 })
 
@@ -183,8 +183,8 @@ function getPostsOfUser(start) {
 
 function geoLocationOfUser (lat, lng, add) {
     map = new google.maps.Map(document.getElementById('addressMap'), {
-        center: new google.maps.LatLng(lat, lng),
         zoom: 6,
+        center: new google.maps.LatLng(lat, lng),
         /* source: https://snazzymaps.com/style/122/flat-map-with-labels */
         styles: [ { "featureType": "water", "elementType": "all", "stylers": [ { "hue": "#7fc8ed" }, { "saturation": 55 }, { "lightness": -6 }, { "visibility": "on" } ] }, { "featureType": "water", "elementType": "labels", "stylers": [ { "hue": "#7fc8ed" }, { "saturation": 55 }, { "lightness": -6 }, { "visibility": "off" } ] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [ { "hue": "#83cead" }, { "saturation": 1 }, { "lightness": -15 }, { "visibility": "on" } ] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [ { "hue": "#f3f4f4" }, { "saturation": -84 }, { "lightness": 59 }, { "visibility": "on" } ] }, { "featureType": "landscape", "elementType": "labels", "stylers": [ { "hue": "#ffffff" }, { "saturation": -100 }, { "lightness": 100 }, { "visibility": "off" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "hue": "#ffffff" }, { "saturation": -100 }, { "lightness": 100 }, { "visibility": "on" } ] }, { "featureType": "road", "elementType": "labels", "stylers": [ { "hue": "#bbbbbb" }, { "saturation": -100 }, { "lightness": 26 }, { "visibility": "on" } ] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [ { "hue": "#ffcc00" }, { "saturation": 100 }, { "lightness": -35 }, { "visibility": "simplified" } ] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "hue": "#ffcc00" }, { "saturation": 100 }, { "lightness": -22 }, { "visibility": "on" } ] }, { "featureType": "poi.school", "elementType": "all", "stylers": [ { "hue": "#d7e4e4" }, { "saturation": -60 }, { "lightness": 23 }, { "visibility": "on" } ] } ],
     });
@@ -203,6 +203,8 @@ function geoLocationOfUser (lat, lng, add) {
     google.maps.event.addListener(marker, 'click', function() {
         infowindow.open(map,marker);
     });
+    
+    google.maps.event.trigger(map, 'resize');
 }
 
 function getAlbumsOfUser (start) {
